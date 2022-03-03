b1bddabaf499103d72a631237028edef5efe75e2
NILE-ERAU <nile.erau@gmail.com>
2022-03-03 05:47:41 +0530

    Updated MiniVGGnet implementation and ShallowNet experimentation

---
+++ machine_learning/cnn/algorithms/__init__.py
@@ -0,0 +1,6 @@
+# import the necessary packages
+from .aspectawarepreprocessor import AspectAwarePreprocessor
+from .imagetoarraypreprocessor import ImageToArrayPreprocessor
+from .simpledatasetloader import SimpleDatasetLoader
+from .minivggnet import MiniVGGNet
+from .shallownet import ShallowNet
