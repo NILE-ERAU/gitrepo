0ea2aaa9c44d8a4e1f5d0efc7b064eaca03be62a
The NILE Project <nile.erau@gmail.com>
2022-04-04 16:34:04 -0700

    Update quad_enc.h

--- NILE_Hardware/quad_enc.h
+++ NILE_Hardware/quad_enc.h
@@ -1,17 +1,18 @@
-#ifndef quad_enc
-#define quad_enc
+//SPI_enc.h - Library for spi encoder
+//Created by Tia, 3/11/22
+
+#ifndef spi_enc
+#define spi_enc
 
 #include <Arduino.h>
 
-class QuadEnc
+class SPI_enc 
 {
-public:
-  QuadEnc(int clk, int dir);
-  void init();
-  int count();
-private:
-  int clk_pin;
-  int dir_pin;
+  public:
+    void init();
+    uint8_t SPIWrite(uint8_t sendByte);
+    uint16_t getPos(); 
+    void setZeroSPI();
 };
 
 #endif
