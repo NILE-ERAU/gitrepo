153590804f43ca9b9bfdb75c75864fad240cb9ee
nphaklides <nphaklides@hotmail.com>
2022-04-18 12:27:20 -0700

    sql_busy handles sql errors I hope

--- Main_Control/main.py
+++ Main_Control/main.py
@@ -14,7 +14,7 @@ import math
 from std_msgs.msg import Int16MultiArray
 from std_msgs.msg import UInt16
 from std_msgs.msg import Float64MultiArray
-from std_msgs.msg import Float64 
+from std_msgs.msg import Float64
 from std_msgs.msg import String
 
 # Import libraries for mySQL website interfacing
@@ -30,12 +30,13 @@ sense = None
 shock = Int16MultiArray(data=[0, 0, 0])
 
 # Run subprocess to open a new terminal and invoke the script 'ros_start' for initiating roscore and a serial node
-subprocess.call(["gnome-terminal", "--","python3", "ros_start.py"])	
+subprocess.call(["gnome-terminal", "--","python3", "ros_start.py"])
 
 Ts = 5
 t = 0
 last_t = 0
 complete_ = True;
+sql_busy = False;
 
 sql.assign_ip()
 
@@ -43,21 +44,25 @@ def publish_web_coords(data):
 	theta = data.data[0]*180/math.pi
 	r = data.data[1]
 	z = data.data[2]
-	time.sleep(0.1)
-
-	sql.publish_pos(theta, r, z);
+    if (not sql_busy):
+        sql_busy = True
+        sql.publish_pos(theta, r, z)
+        sql_busy = False
 
 
 def set_complete_flag(data):
 	complete_ = True
+    while (sql_busy):
+        pass
+    sql_busy = True
 	sql.complete_command(0,0,0,"Success")
-	time.sleep(0.1)
+    sql_busy = False
 	print("Success!")
-	
 
 
-		
-	
+
+
+
 
 def print_val(data):
 	i = 1
@@ -65,68 +70,68 @@ def print_val(data):
 
 # Define function for ROS publishing and subscribing based on website inputs
 def ros_website(execute, theta, r, z, d0, d1, io):
-	
+
 	# Invoke ROS publisher for topic 'home'
 	if execute == "homeTrolley":
 		home_pub.publish("trolley")
-		
+
 	elif execute == "homeVert":
 		home_pub.publish("stepper")
-		
+
 	elif execute == "homeRot":
 		home_pub.publish("rot")
-	# Invoke ROS publisher for topic 'movement'	
+	# Invoke ROS publisher for topic 'movement'
 	elif execute == "moveTo":
 		print('publisher')
 		move_pub.publish(Float64MultiArray(data=[theta,r,z]))
-		
-	# Invoke ROS publisher for topic 'water'	
+
+	# Invoke ROS publisher for topic 'water'
 	elif execute == "water":
 		water_pub.publish(d0)
-	
-	# Invoke ROS publisher for topic 'shock'	
+
+	# Invoke ROS publisher for topic 'shock'
 	elif execute == "hvec":
 		hvec_pub(Int16MultiArray(data=[d0, d1, io]))
-		
+
 	elif execute == "senseSoil":
 		sensor_pub.publish("moisture")
 		print("Read moisture sensor")
-		
+
 	# elif execute == "takeImage":
 		# Capture still image from camera
-		
-		
-	
 
-# Main loop		
+
+
+
+# Main loop
 if __name__ == '__main__':
-    
+
     # Initialize ROS publishers and subscribers
 	# Define ROS publisher for sending string data to topic 'home'
 	home_pub = rospy.Publisher('home', String, queue_size=10)
-		
+
 	# Define ROS publisher for sending string data to topic 'coordinates'
 	move_pub = rospy.Publisher('coordinates', Float64MultiArray, queue_size=10)
-	
-	# Define ROS publisher for sending selector string for sensor to 
+
+	# Define ROS publisher for sending selector string for sensor to
 	# topic 'sensor'
 	sensor_pub = rospy.Publisher('sensor', String, queue_size=10)
 
 	# Define ROS publisher for sending integer data to topic 'water'
 	water_pub = rospy.Publisher('water', UInt16, queue_size=10)
 
-	# Define ROS publisher for sending HVEC activation parameters to 
+	# Define ROS publisher for sending HVEC activation parameters to
 	# topic 'shock'
-	hvec_pub = rospy.Publisher('shock', Int16MultiArray, queue_size=10)    
-	
+	hvec_pub = rospy.Publisher('shock', Int16MultiArray, queue_size=10)
+
 	# Define ROS subscriber for receiving joint encoder values from topic
 	# 'encoder'
 	encoder_sub = rospy.Subscriber('encoder', Float64MultiArray, publish_web_coords)
-	
+
 	# Define ROS subscriber for receiving soil moisture measurements
 	# from topic 'moist'
 	moist_sub = rospy.Subscriber('moist', UInt16, print_val)
-	
+
 	# Define ROS subscriber for receiving soil temperature measurements
 	# from topic 'temp'
 	temp_sub = rospy.Subscriber('temp', Float64, print_val)
@@ -136,7 +141,7 @@ if __name__ == '__main__':
 	# Define ROS subscriber for receiving command completeness
 	# from topic 'complete'
 	complete_sub = rospy.Subscriber('complete', UInt16, set_complete_flag)
-	
+
 	rospy.init_node('publisher', anonymous=True)
 	#rospy.init_node('listener', anonymous=True)
 	#rospy.spin()
@@ -147,20 +152,25 @@ if __name__ == '__main__':
 #     rospy.get_time()
 #     rospy.loginfo(angle)
 #     rospy.loginfo(position)
-    
+
 #     # Clean print by writing over previous message
 #     sys.stdout.write(CURSOR_UP)
 #     sys.stdout.write(ERASE_LINE)
 	time.sleep(8)
 	while True:
-		
+
 		t = time.monotonic()
 		if (t - last_t >= Ts):
-			timetowait = sql.time_until()
-			print(timetowait)
-			if (timetowait >= 0 and complete_):
+            if (not sql_busy):
+                sql_busy = True
+                timetowait = sql.time_until()
+                sql_busy = False
+                print(timetowait)
+			if (timetowait >= 0 and complete_ and not sql_busy):
+                sql_busy = True
 				time.sleep(0.1)
 				queued = sql.pull_next_command()
+                sql_busy = False
 				if (queued[0] == 0):
 					print('SQL Command Retrieve Failure')
 				else:
@@ -170,16 +180,16 @@ if __name__ == '__main__':
 					z_q = queued[5]
 					d0_q = queued[6]
 					d1_q = queued[7]
-					i0_q = queued[8]				
-				
-					# Publish the user input to the ROS topic 
+					i0_q = queued[8]
+
+					# Publish the user input to the ROS topic
 					print(command)
 					complete_ = False
 					ros_website(command, theta_q, r_q, z_q, d0_q, d1_q, i0_q)
-				
 
-				
-			last_t = t		
+
+
+			last_t = t
 
 		# Request input from user
 		var = str(input("Quit program? (y or n): "))
@@ -193,4 +203,3 @@ if __name__ == '__main__':
 	        # os.system("killall rosmaster")
 			print("Program terminated by user")
 			break
-   
